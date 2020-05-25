a1c897a0fecf70621a06860a9ce428b0163cc5ee
jtw023 <jordanwalters023@gmail.com>
2020-05-24 17:09:39 -0700

    cleaned code

--- firebaseWADirectory/index.js
+++ firebaseWADirectory/index.js
@@ -1,66 +1,69 @@
-window.addEventListener('load', () => {
+// defining variables
+window.addEventListener("load", () => {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.temperature-description');
-  let temperatureDegree = document.querySelector('.temperature-degree');
-  let locationTimezone = document.querySelector('.location-timezone');
-  let temperatureSection = document.querySelector('.temperature');
-  const temperatureSpan = document.querySelector('.temperature span');
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
 
+  // if geolocation is working then use that to display info on screen
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position  => {
+    navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = 'https://cors-anywhere.herokuapp.com/';
+      const proxy = "https://cors-anywhere.herokuapp.com/";
 
-      const api = proxy + 'https://api.darksky.net/forecast/8951a04f1393254c1405eb57a2114fd9/' + lat + ',' + long;
+      const api =
+        proxy +
+        "https://api.darksky.net/forecast/8951a04f1393254c1405eb57a2114fd9/" +
+        lat +
+        "," +
+        long;
 
       fetch(api)
-      .then(data => {
-        return data.json();
-      })
-      .then(data => {
-        const { temperature, summary, icon } = data.currently;
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          const { temperature, summary, icon } = data.currently;
 
-        //Set DOM elements from the api
-        temperatureDegree.textContent = Math.floor(temperature);
-        temperatureDescription.textContent = summary;
-        locationTimezone.textContent = data.timezone;
+          //Set DOM elements from the api
+          temperatureDegree.textContent = Math.floor(temperature);
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
 
-        //Formula for Celsius
-        let celsius = (temperature - 32) * (5 / 9);
+          //Formula for Celsius
+          let celsius = (temperature - 32) * (5 / 9);
 
-        //Set icon
-        setIcons(icon, document.querySelector('.icon'));
+          //Set icon
+          setIcons(icon, document.querySelector(".icon"));
 
-        //Change Temperature to Celsius/Farenheit
-        temperatureSection.addEventListener('click', () => {
-          if (temperatureSpan.textContent === 'C') {
-            temperatureSpan.textContent = 'F';
-            temperatureDegree.textContent = Math.floor(temperature);
-          } else {
-            temperatureSpan.textContent = 'C';
-            temperatureDegree.textContent = Math.floor(celsius);
-          }
+          //Change Temperature to Celsius/Farenheit
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "C") {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = Math.floor(temperature);
+            } else {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            }
+          });
         });
-      });
     });
   }
 
+  // icons that I want to display on screen
   function setIcons(icon, iconID) {
-    const skycons = new Skycons({ color: 'white' });
-    const currentIcon = icon.replace(/-/g, '_').toUpperCase();
+    const skycons = new Skycons({
+      color: "white",
+    });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
     skycons.play();
     return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
-
-/*if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(position => {
-  console.log(position)
-} else {
-  console.log('Please allow geolocation for me to find the weather patterns in your area.')
-})
-}
-*/
